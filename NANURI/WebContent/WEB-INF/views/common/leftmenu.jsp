98eac5d795125c5c524d577b1610fd940735c486
limhoooo <dlagh123@gmail.com>
2018-10-12 15:02:34 +0900

    ㄴㄴ

--- NANURI/WebContent/WEB-INF/views/common/leftmenu.jsp
+++ NANURI/WebContent/WEB-INF/views/common/leftmenu.jsp
@@ -2,8 +2,28 @@
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
+<div class="leftmenubtn">
+	
+</div>
+
 <div class="leftmenubox">
-	
-	
+
+	<div class="menu_x">
+		x
+	</div>
 
 </div>
+
+<script type="text/javascript">
+
+	$(".leftmenubtn").click(function(){
+		$(this).fadeOut();
+		$(".leftmenubox").fadeIn();
+	});
+	
+	$(".menu_x").click(function(){
+		$(".leftmenubox").fadeOut();
+		$(".leftmenubtn").fadeIn();
+	});
+
+</script>
--- NANURI/src/com/kh/finalPJ/goods/goodsController.java
+++ NANURI/src/com/kh/finalPJ/goods/goodsController.java
@@ -1,20 +1,42 @@
 package com.kh.finalPJ.goods;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.kh.finalPJ.common.controller;
 
 @Controller
 public class goodsController {
-	private static final Logger logger = LoggerFactory.getLogger(controller.class);
+	private static final Logger logger = LoggerFactory.getLogger(goodsController.class);
 	
-	@RequestMapping(value="rentallist.do",method = RequestMethod.GET)
-	public String test(Model model) throws Exception{
+	@Autowired
+	goodsService goodsService;
+	
+	// getList
+	@RequestMapping(value="rentallist.do",method = {RequestMethod.GET, RequestMethod.POST})
+	public String getList(Model model) throws Exception{
+		
+		List<goodsBbsDto> list = goodsService.getbbs();
+
+		model.addAttribute("bbslist",list);
+		
 		return "rentallist.tiles";
+	}
+	
+	
+	// goodsdetaile view
+	@RequestMapping(value="goodsdetail.do",method = {RequestMethod.GET, RequestMethod.POST})
+	public String goodsdetaile(Model model,int seq) throws Exception{
+		
+
+		model.addAttribute("seq",seq);
+		
+		return "rentaldetaile.tiles";
 	}
 }
