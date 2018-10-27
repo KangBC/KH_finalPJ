5ebcac8e2b092de043a564230db223f52dd57811
BIT\lenovo <Kang.b.c.1208@gmail.com>
2018-10-27 23:39:38 +0900

    안..........돼.........................

--- NANURI/WebContent/WEB-INF/views/rental/order.jsp
+++ NANURI/WebContent/WEB-INF/views/rental/order.jsp
@@ -1,3 +1,7 @@
+<%@page import="com.kh.finalPJ.member.RStatusDto"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
@@ -21,22 +25,35 @@
 </head>
 <body>
 
+<!-- 주문 코드 생성 -->
 <sql:setDataSource
     url="jdbc:oracle:thin:@127.0.0.1:1521:xe"
     driver="oracle.jdbc.driver.OracleDriver"
     user="final"
     password="final"
     var= "conn"/>
+    
+<c:set var="now" value="<%=new java.util.Date()%>" />
+<c:set var="today"><fmt:formatDate value="${now}" pattern="yyyyMMdd" /></c:set>
 
+<sql:query sql="select SEQ_R_STATUS.nextval from dual" var="getseq" dataSource="${conn}" />
+<c:forEach var="seqobj" items="${getseq.rowsByIndex}">
+	<c:set var="seq" value="${seqobj[0]}"/>
+</c:forEach>
+   
+<c:set var="merchant_uid" value="S${today}${seq}"/>
+<input id="merchant_uid" type="hidden" value="merchant_uid">
+
+<!-- 주문 정보 -->
 <div class="startdiv">
 	<div>
 		<h3>주문자 정보</h3> 
 		<div>
-	      <ul style="height: 70px; border: 1px solid #dddddd; background: #f8f8f8; padding: 20px; ">
-	        <li style="float:left; width: 300px; border-left: 1px solid #dddddd;"><b>주문자</b> ${member.name}</li>
-	        <li style="float:left; width: 300px; border-left: 1px solid #dddddd;"><b>연락처</b> ${member.phone}</li>
-	        <li style="float:left; width: 300px; border-left: 1px solid #dddddd;"><b>이메일</b> ${member.email}</li>
-	      </ul>
+	  	<ul style="height: 70px; border: 1px solid #dddddd; background: #f8f8f8; padding: 20px; ">
+	    	<li style="float:left; width: 300px; border-left: 1px solid #dddddd;"><b>주문자</b> ${member.name}</li>
+	    	<li style="float:left; width: 300px; border-left: 1px solid #dddddd;"><b>연락처</b> ${member.phone}</li>
+	    	<li style="float:left; width: 300px; border-left: 1px solid #dddddd;"><b>이메일</b> ${member.email}</li>
+	  	</ul>
 		</div>
 	    <input id="buyer" type="hidden" value="${member.name}">
 	    <input id="buyer_email" type="hidden" value="${member.email}">
@@ -48,7 +65,7 @@
 	    <input id="buyer_address_d" type="hidden" value="${fn:split(member.address,'-')[2]}">
 	</div>
 	<br>
-	
+
 	
 	<div>	
 		<h3>상품 정보</h3>
@@ -63,7 +80,7 @@
 				</tr>
 			</thead>			
 			<tbody>	
-			<c:forEach var="item" items="${goodsList}" varStatus="status" >
+			<c:forEach var="item" items="${goodsList}" varStatus="status">
 				<c:set var="price_one" value="${item.g_price*orderList[status.index].amount*orderList[status.index].month }"/>
 				<c:set var="total" value="${total+price_one}"/>
 				<tr>
@@ -85,8 +102,7 @@
 		<h3 style="display: inline;">배송지 정보 </h3> 
 		<input id="copy" type="checkbox"><label for="copy">주문자와 동일</label><br>
 		<label for="recipient">수령인</label>
-		<input id="recipient" type="text"> <br>
-		
+		<input id="recipient" type="text"> <br>		
 		<label for="phone_container">전화번호</label>
 		<div id="phone_container" style="display: inline-block;">
 		<select id="phone_f">
@@ -102,10 +118,8 @@
 		<br>
 		<label for="address_container">주소</label>	
 		<div id="address_container">
-			<input type="hidden" name="address" id="address" value="">
-					
-			<input type="text" id="address_num" name="address_num" placeholder="Address Number"	readonly="readonly" >
-					
+			<input type="hidden" name="address" id="address" value="">					
+			<input type="text" id="address_num" name="address_num" placeholder="Address Number"	readonly="readonly" >					
 			<button type="button" onclick="sample6_execDaumPostcode()">우편번호 찾기</button>
 			<br>
 			<input type="text" id="address_main" name="address_main" placeholder="Confirm your address" readonly="readonly" >
@@ -113,8 +127,9 @@
 		</div>
 		<br>
 		<br>
-
-
+	</div>
+	
+	<div>
 		<div>
 		<h3>3.결제정보입력</h3>
 		<table cellspacing="0" border="1"  style="border-collapse:collapse; border:1px gray solid">			
@@ -123,8 +138,7 @@
 					<td>상품금액</td>
 					<td>배송비</td>
 					<td>최종결제금액</td>
-				</tr>
-					
+				</tr>					
 				<tr>
 					<td><c:out value="${total}"/></td>
 					<td>4500</td>
@@ -132,17 +146,11 @@
 				</tr>
 			</tbody>
 		</table>
-		<c:set var="now" value="<%=new java.util.Date()%>" />
-		<c:set var="today"><fmt:formatDate value="${now}" pattern="yyyyMMdd" /></c:set>
-		<sql:query sql="select SEQ_R_STATUS.nextval from dual" var="getseq" dataSource="${conn}" />
-		<c:forEach var="seqobj" items="${getseq.rowsByIndex}">
-			<c:set var="seq" value="${seqobj[0]}"/>
-	    </c:forEach>
-
-		<input id="merchant_uid" type="hidden" value="S${today}${seq}">
-		<button id="iamport_module" type="button">결제하기</button>
 		</div>
 	</div>
+
+
+	<button id="iamport_module" type="button">결제하기</button>
 </div>
 </body>
 </html>
--- NANURI/src/com/kh/finalPJ/goods/goodsController.java
+++ NANURI/src/com/kh/finalPJ/goods/goodsController.java
@@ -2,31 +2,25 @@ package com.kh.finalPJ.goods;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.finalPJ.common.orderedDto;
 import com.kh.finalPJ.member.basketDto;
 import com.kh.finalPJ.member.memberDto;
-import com.kh.finalPJ.member.memberService;
 import com.kh.finalPJ.review.reviewDto;
-
 
 @Controller
 public class goodsController {
@@ -36,21 +30,20 @@ public class goodsController {
 	goodsService goodsService;
 
 	// getList
-	@RequestMapping(value="rentallist.do",method = {RequestMethod.GET, RequestMethod.POST})
-	public String getList(Model model,HttpServletRequest request) throws Exception{
+	@RequestMapping(value = "rentallist.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String getList(Model model, HttpServletRequest request) throws Exception {
 
 		List<goodsBbsDto> list = goodsService.getbbslength();
 
-		model.addAttribute("bbslist",list);
+		model.addAttribute("bbslist", list);
 
 		return "rentallist.tiles";
 
 	}
 
-
 	// goodsdetaile view
-	@RequestMapping(value="goodsdetail.do",method = {RequestMethod.GET, RequestMethod.POST})
-	public String goodsdetaile(Model model,int seq,String g_code,HttpServletRequest req) throws Exception{
+	@RequestMapping(value = "goodsdetail.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String goodsdetaile(Model model, int seq, String g_code, HttpServletRequest req) throws Exception {
 
 		// readcount
 		goodsService.readcount(seq);
@@ -59,15 +52,14 @@ public class goodsController {
 		// g_code 연결(rivew,qna)
 		List<reviewDto> code = goodsService.getreview_qna(g_code);
 
-
 		for (int i = 0; i < code.size(); i++) {
 			System.out.println(code.get(i));
 		}
 
 		System.out.println(g_code);
 
-		model.addAttribute("detail",dto);
-		model.addAttribute("reviewDto",code);
+		model.addAttribute("detail", dto);
+		model.addAttribute("reviewDto", code);
 		return "rentaldetaile.tiles";
 	}
 
@@ -80,49 +72,42 @@ public class goodsController {
 		String lists = req.getParameter("lists");
 		String category = req.getParameter("category");
 
-
 		System.out.println(category);
 		System.out.println(title);
 		System.out.println(lists);
 
 		Map<Object, Object> map = new HashMap<>();
 
-		List<goodsBbsDto> list = goodsService.findgoods(title,lists,category);
+		List<goodsBbsDto> list = goodsService.findgoods(title, lists, category);
 
 		map.put("list", list);
-
 
 		return map;
 
 	}
 
-	/*페이징 인덱스*/
+	/* 페이징 인덱스 */
 	@ResponseBody
 	@RequestMapping(value = "findlist.do", method = RequestMethod.POST)
 	public Map<Object, Object> findlist(HttpServletRequest req) throws Exception {
-
 
 		String startindex = req.getParameter("startindex");
 		String endindex = req.getParameter("endindex");
 		String category = req.getParameter("category");
 
-
-
-
-		List<goodsBbsDto> list = goodsService.getbbs(startindex,endindex,category);
+		List<goodsBbsDto> list = goodsService.getbbs(startindex, endindex, category);
 		Map<Object, Object> map = new HashMap<>();
 
 		map.put("list", list);
-
 
 		return map;
 
 	}
 
-	/*장바구니 insert*/
+	/* 장바구니 insert */
 	@ResponseBody
-	@RequestMapping(value="basketinsert.do",method = RequestMethod.POST)
-	public boolean basketinsert(Model model,HttpServletRequest req) throws Exception{
+	@RequestMapping(value = "basketinsert.do", method = RequestMethod.POST)
+	public boolean basketinsert(Model model, HttpServletRequest req) throws Exception {
 
 		String id = req.getParameter("id");
 		String g_code = req.getParameter("gcode");
@@ -130,10 +115,8 @@ public class goodsController {
 		int amount = (Integer.parseInt(req.getParameter("amount")));
 		int resultnum = (Integer.parseInt(req.getParameter("resultnum")));
 
-
-
-
-		//System.out.println(resultnum + "ddddddddddddddddddddddddddddddddddddddddddd");
+		// System.out.println(resultnum +
+		// "ddddddddddddddddddddddddddddddddddddddddddd");
 
 		basketDto dto = new basketDto();
 		dto.setId(id);
@@ -146,106 +129,99 @@ public class goodsController {
 
 		return goodsService.basketinsert(dto);
 
-
-
 	}
 
-	/*카테고리*/
-	@RequestMapping(value="rental_category.do",method = {RequestMethod.GET, RequestMethod.POST})
-	public String rental_category(Model model,String division) throws Exception{
-
+	/* 카테고리 */
+	@RequestMapping(value = "rental_category.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String rental_category(Model model, String division) throws Exception {
 
 		List<goodsBbsDto> list = null;
 		String title = null;
 		// 유아
-		if(division.equals("AC")) {
+		if (division.equals("AC")) {
 			list = goodsService.getbbscategory(division);
 			title = "유아동";
 		}
 		// 레저
-		else if(division.equals("BC")) {
+		else if (division.equals("BC")) {
 			list = goodsService.getbbscategory(division);
 			title = "레저";
 		}
 		// 패션
-		else if(division.equals("CC")) {
+		else if (division.equals("CC")) {
 			list = goodsService.getbbscategory(division);
 			title = "패션";
 		}
 		// 리빙
-		else if(division.equals("DC")) {
+		else if (division.equals("DC")) {
 			list = goodsService.getbbscategory(division);
 			title = "리빙";
 		}
 
 		System.out.println(list.size());
 
-		model.addAttribute("title",title);
-		model.addAttribute("bbslist",list);
+		model.addAttribute("title", title);
+		model.addAttribute("bbslist", list);
 
 		return "rentallist.tiles";
 
 	}
 
-
-	@RequestMapping(value="order.do",method = {RequestMethod.GET, RequestMethod.POST})
-	public String order(Model model,orderedDto ordered) throws Exception{
+	@RequestMapping(value = "order.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String order(Model model, orderedDto ordered) throws Exception {
 		String id = ordered.getId();
 		String g_code = ordered.getG_code();
 
 		memberDto member = goodsService.selectMember(id);
 		goodsDto goods = goodsService.selectGoods(g_code);
 
-		System.out.println(member.getId() + "00000000000000000000000000000000000000");
-		System.out.println(goods.getG_code() + "00000000000000000000000000000000000000");
-
 		List<goodsDto> goodsList = new ArrayList<>();
 		goodsList.add(goods);
 		List<orderedDto> orderList = new ArrayList<>();
 		orderList.add(ordered);
 
-		model.addAttribute("member",member);
-		model.addAttribute("goodsList",goodsList);
-		model.addAttribute("orderList",orderList);
+		model.addAttribute("member", member);
+		model.addAttribute("goodsList", goodsList);
+		model.addAttribute("orderList", orderList);
 		return "order.tiles";
 	}
 
+	@ResponseBody
+	@RequestMapping(value = "orderAf.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String orderAf(Model model, @RequestBody Map<String, Object> map) throws Exception {
+		return null;
+	}
+
 	// headselect
-		@RequestMapping(value="headselect.do",method = {RequestMethod.GET, RequestMethod.POST})
-		public String headselect(Model model,String title) throws Exception{
+	@RequestMapping(value = "headselect.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String headselect(Model model, String title) throws Exception {
 
+		List<goodsBbsDto> list = goodsService.headselect(title);
 
-			List<goodsBbsDto> list = goodsService.headselect(title);
+		System.out.println(list.size());
 
-			System.out.println(list.size());
+		String head = "검색 된 상품";
+		model.addAttribute("title", head);
+		model.addAttribute("bbslist", list);
+		return "rentallist_select.tiles";
 
-			String head = "검색 된 상품";
-			model.addAttribute("title",head);
-			model.addAttribute("bbslist",list);
-			return "rentallist_select.tiles";
-
-		}
+	}
 
 	// headselectajax
-		@ResponseBody
-		@RequestMapping(value="headselectajax.do",method = RequestMethod.POST)
-		public Map<Object, Object> headselectajax(Model model,HttpServletRequest req) throws Exception{
-			
-			String title = req.getParameter("title");
+	@ResponseBody
+	@RequestMapping(value = "headselectajax.do", method = RequestMethod.POST)
+	public Map<Object, Object> headselectajax(Model model, HttpServletRequest req) throws Exception {
 
+		String title = req.getParameter("title");
 
+		List<goodsBbsDto> list = goodsService.headselectajax(title);
 
-			List<goodsBbsDto> list = goodsService.headselectajax(title);
+		System.out.println(list.size());
+		Map<Object, Object> map = new HashMap<>();
 
-			System.out.println(list.size());
-			Map<Object, Object> map = new HashMap<>();
+		map.put("list", list);
 
-			map.put("list", list);
-
-
-			return map;
-
-
-		}
+		return map;
+	}
 
 }
