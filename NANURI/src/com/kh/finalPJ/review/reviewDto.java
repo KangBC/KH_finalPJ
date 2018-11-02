e719235a7fddeff728464c3f95824c3a9d1c4aa5
HYEONSEOK\HYEONSEOK <gksgustjr727@naver.com>
2018-11-02 10:24:51 +0900

    마이페이지 -> 리뷰디테일 사진누락 수정 -현석

--- NANURI/src/com/kh/finalPJ/review/reviewDto.java
+++ NANURI/src/com/kh/finalPJ/review/reviewDto.java
@@ -38,16 +38,6 @@ public class reviewDto implements Serializable{
 	public reviewDto() {
 		
 	}
-	
-	public reviewDto(int seq, String id, String title, String content, String wdate, int del) {
-		super();
-		this.seq = seq;
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.wdate = wdate;
-		this.del = del;
-	}
 
 	public reviewDto(int seq, String id, String title, String content, String wdate, int del, String g_code, int rating,
 			String g_img) {
@@ -63,12 +53,66 @@ public class reviewDto implements Serializable{
 		this.g_img = g_img;
 	}
 
-	public String getG_img() {
-		return g_img;
+	@Override
+	public String toString() {
+		return "reviewDto [seq=" + seq + ", id=" + id + ", title=" + title + ", content=" + content + ", wdate=" + wdate
+				+ ", del=" + del + ", g_code=" + g_code + ", rating=" + rating + ", g_img=" + g_img + "]";
 	}
 
-	public void setG_img(String g_img) {
-		this.g_img = g_img;
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWdate() {
+		return wdate;
+	}
+
+	public void setWdate(String wdate) {
+		this.wdate = wdate;
+	}
+
+	public int getDel() {
+		return del;
+	}
+
+	public void setDel(int del) {
+		this.del = del;
+	}
+
+	public String getG_code() {
+		return g_code;
+	}
+
+	public void setG_code(String g_code) {
+		this.g_code = g_code;
 	}
 
 	public int getRating() {
@@ -79,54 +123,12 @@ public class reviewDto implements Serializable{
 		this.rating = rating;
 	}
 
-	public int getSeq() {
-		return seq;
-	}
-	public void setSeq(int seq) {
-		this.seq = seq;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getWdate() {
-		return wdate;
-	}
-	public void setWdate(String wdate) {
-		this.wdate = wdate;
-	}
-	public int getDel() {
-		return del;
-	}
-	public void setDel(int del) {
-		this.del = del;
-	}
-	public String getG_code() {
-		return g_code;
-	}
-	public void setG_code(String g_code) {
-		this.g_code = g_code;
+	public String getG_img() {
+		return g_img;
 	}
 
-	@Override
-	public String toString() {
-		return "reviewDto [seq=" + seq + ", id=" + id + ", title=" + title + ", content=" + content + ", wdate=" + wdate
-				+ ", del=" + del + ", g_code=" + g_code + "]";
+	public void setG_img(String g_img) {
+		this.g_img = g_img;
 	}
-	
 	
 }
